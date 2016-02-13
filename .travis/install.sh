186bce0f21695d58d7345f8398120b17b67c81d4
Andrea Barberio <barberio@fb.com>
2016-02-13 17:31:01 +0000

    Refactored Travis configuration into separate scripts

---
+++ .travis/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]
+then
+    if [ "$CXX" = "g++" ]
+    then
+        sudo apt-get install -qq g++-4.8
+        export CXX="g++-4.8"
+    elif [ "$CXX" = "clang" ]
+    then
+        sudo apt-get install --allow-unauthenticated -qq clang-3.4
+        export CXX="clang++-3.4"
+    fi
+fi
