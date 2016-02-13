b0d5f71fd25ef7cb6419256cb01ec75cebbd7fd9
Andrea Barberio <barberio@fb.com>
2016-02-13 17:51:32 +0000

    Enforced compiler version in Travis build

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,10 +5,16 @@ then
     if [ "$CXX" = "g++" ]
     then
         sudo apt-get install -qq g++-4.8
-        export CXX="g++-4.8"
     elif [ "$CXX" = "clang" ]
     then
         sudo apt-get install --allow-unauthenticated -qq clang-3.4
-        export CXX="clang++-3.4"
     fi
+fi
+
+if [ "$CXX" = "g++" ]
+then
+    export CXX="g++-4.8"
+elif [ "$CXX" = "clang" ]
+then
+    export CXX="clang++-3.4"
 fi
