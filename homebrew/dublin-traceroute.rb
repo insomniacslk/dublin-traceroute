dbc4fa92f07b84d5adb3fe97d96233162fee3bce
insomniac <insomniac@slackware.it>
2016-09-19 02:25:59 +0100

    Bumped version to 0.3.1; updated homebrew formula

--- homebrew/dublin-traceroute.rb
+++ homebrew/dublin-traceroute.rb
@@ -1,8 +1,8 @@
 class DublinTraceroute < Formula
   desc "NAT-aware multipath tracerouting tool"
   homepage "https://dublin-traceroute.net"
-  url "https://github.com/insomniacslk/dublin-traceroute/archive/v0.3.tar.gz"
-  sha256 "12b01006043dd5d34b20446367cc83ac07cc3a0ab8aedcf56bc35d6f6587c5c9"
+  url "https://github.com/insomniacslk/dublin-traceroute/archive/v0.3.1.tar.gz"
+  sha256 "d6e72d17cd0a5ea3f654acd277acb44e80d65d370acea36ae4d98e74d8ee9911"
   head "https://github.com/insomniacslk/dublin-traceroute.git"
 
   depends_on "cmake" => :build
