47eebc0d0e2e431ea32db712b46894c74fa2eafe
Andrea Barberio <insomniac@slackware.it>
2019-08-14 23:10:22 +0100

    Added RouTest
    Added program to do integration testing of dublin-traceroute using
    NFQUEUE
    
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

---
+++ go/dublintraceroute/cmd/routest/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+
+if [ $UID -ne 0 ]
+then
+    # shellcheck disable=SC2086,SC2068
+    sudo $0 $@
+    exit $?
+fi
+
+QUEUE=100
+iptables -A OUTPUT -p udp --dport 33434:33634 -d 8.8.8.8 -j NFQUEUE --queue-num "$QUEUE"
+# then run ./routest -q $QUEUE -c <your-config.json> -i <interface>
