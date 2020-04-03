8e3d3504d4a6216f7181d12c351068756218e9ee
stickler-ci <support@stickler-ci.com>
2020-04-03 09:45:07 +0000

    Fixing style errors.

--- homebrew/dublin-traceroute.rb
+++ homebrew/dublin-traceroute.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 # You need the latest XCode for this formula to build
 class DublinTraceroute < Formula
-  desc "NAT-aware multipath tracerouting tool"
-  homepage "https://dublin-traceroute.net"
-  url "https://github.com/insomniacslk/dublin-traceroute/archive/v0.4.2.tar.gz"
-  sha256 "255980a630fb0d8b1cac270a656d6236bfddc5ba253bda4b898302918caf76d1"
-  head "https://github.com/insomniacslk/dublin-traceroute.git"
+  desc 'NAT-aware multipath tracerouting tool'
+  homepage 'https://dublin-traceroute.net'
+  url 'https://github.com/insomniacslk/dublin-traceroute/archive/v0.4.2.tar.gz'
+  sha256 '255980a630fb0d8b1cac270a656d6236bfddc5ba253bda4b898302918caf76d1'
+  head 'https://github.com/insomniacslk/dublin-traceroute.git'
 
-  depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
+  depends_on 'cmake' => :build
+  depends_on 'pkg-config' => :build
   # this should be in the libtins formula
-  depends_on "libpcap"
-  depends_on "libtins"
-  depends_on "jsoncpp"
+  depends_on 'libpcap'
+  depends_on 'libtins'
+  depends_on 'jsoncpp'
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    system 'cmake', '.', *std_cmake_args
+    system 'make', 'install'
   end
 
   test do
